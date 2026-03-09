da26ded266aa94cc3f3047bc608388404458f2e1
Anujpatnaik9 <anujpatnaik9@gmail.com>
2026-03-09 15:36:53 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Your app is working!"
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
